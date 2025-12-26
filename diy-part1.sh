2d30bf42394ae347db28c3622f38135f731e8b3d
uranai <95555407+haoeixc@users.noreply.github.com>
2025-12-26 16:53:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
-git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
-git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+#git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+#git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
+#git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
+#git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone https://github.com/sirpdboy/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/ntbowen/fastfetch-openwrt.git package/fastfetch
